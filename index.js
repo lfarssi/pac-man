9813321b76a1f91af55cd4a81835fcb02c10dd84
f <medlfarssi10@gmail.com>
2025-03-30 02:00:38 +0000

    add collision

--- index.js
+++ index.js
@@ -21,15 +21,38 @@ let powerPillTimer= null
 
 
 function gameOver(pacman, grid){
-
+    document.removeEventListener('keydown',e=>pacman.handleKeyInput(e,gameBoard.objectExists))
+    gameBoard.showGameStatus(isWinner)
+    clearInterval(timer)
+    startBtn.classList.remove('hide')
 }
 function checkCollision(pacman, ghosts){
-
+    const collidedGhost=ghosts.find(ghost=>pacman.pos===ghost.pos)
+    if (collidedGhost){
+        if(pacman.powerPillActive){
+            gameBoard.removeObject(collidedGhost.pos, [
+                object_type.GHOST,
+                object_type.SCARED,
+                collidedGhost.name
+            ])
+            collidedGhost.pos=collidedGhost.startPos
+            score+=100
+        }else{
+            gameBoard.removeObject(pacman.pos,[object_type.PACMAN])
+            gameBoard.rotatePacMan(pacman.pos,0)
+            gameOver(pacman,gameGrid)
+        }
+    }
 }
 function gameLoop(pacman, ghosts){
     gameBoard.moveCharacter(pacman)
+    checkCollision(pacman,ghosts)
 
     ghosts.forEach(ghost=>gameBoard.moveCharacter(ghost))
+    checkCollision(pacman,ghosts)
+
+
+    
 }
 function startGame(){
     isWinner=false
