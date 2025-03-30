a0c401f01144c8b435d5e8f09ef7644b1ee6cfa4
f <medlfarssi10@gmail.com>
2025-03-30 01:08:17 +0000

    add ghosts

--- Ghost.js
+++ Ghost.js
@@ -1,4 +1,4 @@
-import { DIRECTIONS, object_type } from "./setup";
+import { DIRECTIONS, object_type } from "./setup.js";
 
 class Ghost{
     constructor(speed=5,startPos,movement, name){
@@ -12,4 +12,32 @@ class Ghost{
         this.isScared=false
         this.rotation=false
     }
+    shouldMove(){
+        if(this.timer===this.speed){
+            this.timer=0
+            return true
+        }
+        this.timer++
+        return false
+    }
+    getNextMove(objectExists){
+        const   {nextMovePos, direction}=this.movement(
+            this.pos,
+            this.dir,
+            objectExists
+        )
+        return {nextMovePos, direction}
+    }
+    makeMove(){
+        const classesToRemove=[object_type.GHOST,object_type.SCARED, this.name]
+        let classesToAdd=[object_type.GHOST, this.name]
+        if(this.isScared)classesToAdd=[...classesToAdd, object_type.SCARED]
+        return {classesToRemove, classesToAdd}
+    }
+    setNewPos(nextMovePos, direction){
+        this.pos=nextMovePos
+        this.dir=direction
+    }
 }
+
+export default Ghost
